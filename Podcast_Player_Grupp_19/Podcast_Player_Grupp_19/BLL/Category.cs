7025f381ef0229293b33d1e057fe6b81acd7fd22
hsagstrom <hugsav181@edunet.oru.se>
2018-10-24 16:12:13 +0200

    Ny .cs för ItemList

--- Podcast_Player_Grupp_19/Podcast_Player_Grupp_19/BLL/Category.cs
+++ Podcast_Player_Grupp_19/Podcast_Player_Grupp_19/BLL/Category.cs
@@ -16,37 +16,5 @@ namespace Podcast_Player_Grupp_19.BLL
         {
             CategoryName = name;
         }
-
-    }
-    public class ItemList<T> : IList<T>
-    {
-        public List<T> List { get; set; }
-
-        public ItemList(T item)
-        {
-            List = new List<T>();
-        }
-        public void AddToList(T item)
-        {
-            if (!List.Contains(item))
-            {
-                List.Add(item);
-            }
-            else
-            {
-                MessageBox.Show("Listan " + List + " innehåller redan " + item + " .", "Error Message");
-            }
-        }
-        public void RemoveFromList(T item)
-        {
-            if (List.Contains(item))
-            {
-                List.Remove(item);
-            }
-            else
-            {
-                MessageBox.Show("Det gick inte att ta bort " +item+" för att listan inte innehåller " + item + ".", "Error Message");
-            }
-        }
     }
 }
---
+++ Podcast_Player_Grupp_19/Podcast_Player_Grupp_19/BLL/ItemList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Podcast_Player_Grupp_19.BLL
+{
+
+    public class ItemList<T> : IList<T>
+    {
+        public List<T> List { get; set; }
+
+        public ItemList(T item)
+        {
+            List = new List<T>();
+        }
+        public void AddToList(T item)
+        {
+            if (!List.Contains(item))
+            {
+                List.Add(item);
+            }
+            else
+            {
+                MessageBox.Show("Listan " + List + " innehåller redan " + item + " .", "Error Message");
+            }
+        }
+        public void RemoveFromList(T item)
+        {
+            if (List.Contains(item))
+            {
+                List.Remove(item);
+            }
+            else
+            {
+                MessageBox.Show("Det gick inte att ta bort " + item + " för att listan inte innehåller " + item + ".", "Error Message");
+            }
+        }
+    }
+}
