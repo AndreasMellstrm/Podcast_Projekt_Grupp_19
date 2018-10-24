a252265dd87bc0349da6c4310f9cfb729813342f
hsagstrom <hugsav181@edunet.oru.se>
2018-10-24 10:55:20 +0200

    Skapat klassen category

---
+++ Podcast_Player_Grupp_19/Podcast_Player_Grupp_19/BLL/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podcast_Player_Grupp_19.BLL
+{
+    public enum category
+    {
+
+    }
+    class Category
+    {
+        
+
+    }
+}
