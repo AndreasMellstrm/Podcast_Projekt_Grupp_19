056c9401c2bb6d57598c31f7cb47a8668393eccd
ludvig lundh <l.lundh@hotmail.com>
2018-10-29 12:18:40 +0100

    skapade klassen serializer

---
+++ Podcast_Player_Grupp_19/Podcast_Player_Grupp_19/BLL/Serialize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Podcast_Player_Grupp_19.BLL {
+    class Serialize {
+        public void serializer() {
+            var doc = XmlWriter.Create("test.xml");
+            doc.WriteStartDocument();
+
+            doc.WriteString("testtesttest");
+
+            doc.Close();
+        }
+    }
+}
