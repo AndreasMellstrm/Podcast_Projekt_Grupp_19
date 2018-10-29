76ecb00c01fec3b711f753102beec157722c694e
ludvig lundh <l.lundh@hotmail.com>
2018-10-29 12:38:38 +0100

    serialize class

--- Podcast_Player_Grupp_19/Podcast_Player_Grupp_19/BLL/Serialize.cs
+++ Podcast_Player_Grupp_19/Podcast_Player_Grupp_19/BLL/Serialize.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Serialization;
 
 namespace Podcast_Player_Grupp_19.BLL {
     class Serialize {
+        StreamWriter myWriter = new StreamWriter("xmlFile.xml");
         public void serializer() {
-            var doc = XmlWriter.Create("test.xml");
-            doc.WriteStartDocument();
 
-            doc.WriteString("testtesttest");
 
-            doc.Close();
+
+
         }
     }
 }
